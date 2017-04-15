e0bdf8788047a2a6e96a99ae70913a7ab9af91eb
joeywillempie <joey-willemse@hotmail.com>
2017-04-15 13:12:25 +0200

    Delete SpeakGoodBye.js

--- site/SpeakGoodBye.js
+++
@@ -1,8 +0,0 @@
-(function () {
-	var speakWord = "GoodBye";
-	var byeSpeaker = {};
-byeSpeaker.speak = function(byeSpeaker) {
-  console.log(speakWord + byeSpeaker);
-}
-window.byeSpeaker = byeSpeaker;
-})(window);
